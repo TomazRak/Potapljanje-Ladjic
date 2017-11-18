96556223d0f457cb88c6b9a63b20916a237eb70c
TomazRak <rak.tomaz@gmail.com>
2017-11-18 13:37:49 +0100

    Edit Grid.cs

--- Potapljanje_Ladjic/Assets/Grid.cs
+++ Potapljanje_Ladjic/Assets/Grid.cs
@@ -25,13 +25,13 @@ public class Grid : MonoBehaviour {
         InitCells();
     }
 
-    void InitCells(); {
+    void InitCells() {
         GameObject cellObject = new GameObject();
 		//creates an empty object and adds a sprite renderer component -> set the sprite to cellSprite
-		cellObject.AddComponent<SpriteRender>().sprite = cellSprite;
+		cellObject.AddComponent<SpriteRenderer>().sprite = cellSprite;
 		
 		//catch the size of sprite
-		cellSize = cellSprite.bonus.size;
+		cellSize = cellSprite.bounds.size;
 		
 		//get the new cell size by -> adjust the size off the cells to fit the size of the grid
 		Vector2 newCellSize = new Vector2(gridSize.x / (float) cols, gridSize.y / gridSize.y / (float)rows);
@@ -40,16 +40,16 @@ public class Grid : MonoBehaviour {
 		cellScale.x = newCellSize.x / cellSize.x;
 		cellScale.y = newCellSize.y / cellSize.y;
 
-        cellSize = new CellSize; //the size will be replaced by the new computer size, we just used cellSize for computing the scale
+        cellSize = newCellSize; //the size will be replaced by the new computer size, we just used cellSize for computing the scale
 
-        cellObject.transforem.localScale = new Vector2(CellScale.x, cellScale.y);
+        cellObject.transform.localScale = new Vector2(cellScale.x, cellScale.y);
 
         //fill the grid whit cells by using Instntiate
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
                 Vector2 pos = new Vector2(col * cellSize.x + gridOffset.x + transform.position.x, row * cellSize.y + gridOffset.y + transform.position.v);
                 //instantiate the game object, at position pos, with rotation set identity
-                GameObject c0 = Instantiate(cellObject, pos, Quaternion.identify) as GameObject;
+                GameObject c0 = Instantiate(cellObject, pos, Quaternion.identity) as GameObject;
                 
                 //set the parent of the cell to GRID so you can move the cells together with the grid
                 c0.transform.parent = transform;
