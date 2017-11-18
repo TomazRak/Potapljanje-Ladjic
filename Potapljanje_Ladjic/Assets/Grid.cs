fd2ac1ccb7edaafd960d76d40f214fe9453342e3
TomazRak <32834895+TomazRak@users.noreply.github.com>
2017-11-18 12:36:08 +0100

    Add Grid

---
+++ Potapljanje_Ladjic/Assets/Grid.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+
+    //grid dimenzije
+    [SerializeField]
+    private int rows;
+    [SerializeField]
+    private int cols;
+    [SerializeField]
+    private Vector2 gridSize;
+    [SerializeField]
+    private Vector2 cellOffset; //offsets celice
+
+    //
+    [SerializeField]
+    private Sprite cellSprite;
+    private Vector2 cellSize;
+    private Vector2 cellScale;
+
+    void Start() {
+        InitCells();
+    }
+
+    void InitCells(); {
+        GameObject cellObject = new GameObject();
+    }
+}
