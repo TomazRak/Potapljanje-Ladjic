734e5a1b4cff048583f645b2593e08130f78550a
TomazRak <32834895+TomazRak@users.noreply.github.com>
2017-11-18 14:27:06 +0100

    Edit Grid.cs

--- Potapljanje_Ladjic/Assets/Grid.cs
+++ Potapljanje_Ladjic/Assets/Grid.cs
@@ -51,7 +51,9 @@ public class Grid : MonoBehaviour {
         //fill the grid whit cells by using Instntiate
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
-                Vector2 pos = new Vector2(col * cellSize.x + gridOffset.x + transform.position.x, row * cellSize.y + gridOffset.y + transform.position.y);﻿
+                Vector2 pos = new Vector2(col * cellSize.x + gridOffset.x, row * cellSize.y + gridOffset.y);
+                /*replaced*/
+                //Vector2 pos = new Vector2(col * cellSize.x + gridOffset.x + transform.position.x, row * cellSize.y + gridOffset.y + transform.position.y);﻿
                 //instantiate the game object, at position pos, with rotation set identity
                 GameObject c0 = Instantiate(cellObject, pos, Quaternion.identity) as GameObject;
                 
@@ -67,6 +69,6 @@ public class Grid : MonoBehaviour {
     //so you can see the width and height of the grid on editor
     void OnDrawGizmos()
     {
-        Gizmos.DrawCube(transform.position, gridSize);
+        Gizmos.DrawWireCube(transform.position, gridSize);
     }
 }
