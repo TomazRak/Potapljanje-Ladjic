3d009ac31d1aa81a979377ff2c1b8c3cd72d83d5
TomazRak <rak.tomaz@gmail.com>
2017-11-24 11:57:28 +0100

    add GameController

---
+++ Potapljanje_Ladjic/Assets/Scripts/GameController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour { 
+    public GameObject board1 = GameObject.FindWithTag("Board player1");
+    public GameObject board2 = GameObject.FindWithTag("Board player2");
+}
