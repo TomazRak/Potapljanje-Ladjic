59ad60812c819509198a434363866a37e4cef287
TomazRak <32834895+TomazRak@users.noreply.github.com>
2017-11-25 10:45:14 +0100

    Revert "Edit grid pos"
    This reverts commit 9836231e8e3825568f254fcfaf07489f9e2b21b4.

--- Potapljanje_Ladjic/Assets/Grid.cs
+++ Potapljanje_Ladjic/Assets/Grid.cs
@@ -51,9 +51,9 @@ public class Grid : MonoBehaviour {
         //fill the grid whit cells by using Instntiate
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
-                //Vector2 pos = new Vector2(col * cellSize.x + gridOffset.x, row * cellSize.y + gridOffset.y);//stari
+                Vector2 pos = new Vector2(col * cellSize.x + gridOffset.x, row * cellSize.y + gridOffset.y);
                 /*replaced*/
-                Vector2 pos = new Vector2(col * cellSize.x + gridOffset.x + transform.position.x, row * cellSize.y + gridOffset.y + transform.position.y);﻿//novi
+                //Vector2 pos = new Vector2(col * cellSize.x + gridOffset.x + transform.position.x, row * cellSize.y + gridOffset.y + transform.position.y);﻿
                 //instantiate the game object, at position pos, with rotation set identity
                 GameObject c0 = Instantiate(cellObject, pos, Quaternion.identity) as GameObject;
                 
